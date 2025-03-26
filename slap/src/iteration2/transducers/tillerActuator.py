8fa3951bd2621cceca8bc977ff52c267a564a6ff
SailoBam <francisbamsey@gmail.com>
2025-03-26 19:07:40 +0000

    reemplement motor control

--- slap/src/iteration2/transducers/tillerActuator.py
+++ slap/src/iteration2/transducers/tillerActuator.py
@@ -1,6 +1,11 @@
 from control.boatSim import BoatSim
 import time
-
+try:
+    import RPi.GPIO as GPIO
+    IS_RPI = True
+except (ImportError, ModuleNotFoundError, RuntimeError):
+    print("Not running on a Raspberry Pi")
+    IS_RPI = False
 
 
 
@@ -10,14 +15,16 @@ SERVO_RANGE = 270
 class TillerActuator():
     
     def __init__(self):
+        if IS_RPI:
+            servoPIN = 18
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(servoPIN, GPIO.OUT)
 
-        """servoPIN = 18
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(servoPIN, GPIO.OUT)
-
-        self.p = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
-        self.p.start(0.5) # Initialization
-        self.cycle = 0"""
+            self.p = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
+            self.p.start(0.5) # Initialization
+            self.cycle = 0
+        else:
+            self.cycle = 0
  
     def setBoatSim(self, boat_sim: BoatSim):
         self.boat_sim = boat_sim
@@ -28,7 +35,8 @@ class TillerActuator():
         # to find the next angle on discrete time
         angle = RUDDER_COEFFICIENT * turn_mag
         self.boat_sim.setRudderAngle(angle)
-        #self.setServo(turn_mag)
+        if IS_RPI:
+            self.setServo(turn_mag)
         return angle
     
     def setServo(self, turn_mag: float):
